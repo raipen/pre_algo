17a56ea527557782f03cb421bed14b65ff2a57ab
Jiwon <raipen@knu.ac.kr>
2022-02-07 13:18:46 +0900

    220207jiwon

--- ji1/baekjoon/test/main.c
+++ ji1/baekjoon/test/main.c
@@ -1,51 +1,88 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-typedef struct {
-    int blue;
-    int white;
-} paper;
+int m[1000000];
+int arr[1000000];
+int arr2[1000000];
+int count = 0;
 
-int matrix[128][128];
 
-paper check(int col, int row, int size);
+void quicksort(int arr[], int left, int right) {
+    int L = left, R = right;
+    int temp;
+    int pivot = arr[(left + right) / 2];
 
-int main(void)
-{
-    int size;
-    scanf("%d", &size);
+    while (L <= R) {
+        while (arr[L] < pivot)
+            L++;
+        while (arr[R] > pivot)
+            R--;
 
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            scanf(" %d", &matrix[i][j]);
 
-    paper N = check(0, 0, size);
-    printf("%d\n%d", N.white, N.blue);
+        if (L <= R) {
+            if (L != R) {
+                temp = arr[L];
+                arr[L] = arr[R];
+                arr[R] = temp;
+            }
+            L++; R--;
 
-    return 0;
+
+        }
+    }
+
+    if (left < R)
+        quicksort(arr, left, R);
+    if (L < right)
+        quicksort(arr, L, right);
 }
 
-paper check(int col, int row, int size)
-{
-    paper cnt = { 0,0 };
+int main() {
+    int n, num, j;
+    int flag;
+    scanf("%d", &n);
 
-    if (size == 1) {     // 재귀 종료문
-            // blue = 1, white = 0
-            cnt.blue = matrix[col][row];
-            cnt.white = !matrix[col][row];
-    }
-    else
-    {
-        int newSize = size / 2;
-        paper check1 = check(col, row, newSize);   // check1을 2분면으로 1, 3, 4분면의 색을 확인
-        paper check2 = check(col + newSize, row, newSize);
-        paper check3 = check(col, row + newSize, newSize);
-        paper check4 = check(col + newSize, row + newSize, newSize);
-        cnt.blue = check1.blue + check2.blue + check3.blue + check4.blue;
-        cnt.white = check1.white + check2.white + check3.white + check4.white;
-        if (!cnt.white) cnt.blue = 1;
-        if (!cnt.blue) cnt.white = 1;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num);
+        m[i] = arr[i] = num;
     }
 
-    return cnt;
+    quicksort(arr, 0, n - 1);
+
+    arr2[count++] = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr2[count - 1] == arr[i]) continue;
+            arr2[count++] = arr[i];
+    }
+
+    /*printf("arr1: ");
+    for (int i = 0; i < n; i++) {
+       printf("%d |", arr[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < count; i++) {
+       printf("%d |", arr2[i]);
+    }*/
+
+    for (int i = 0; i < n; i++) {
+        int left = 0;
+        int right = count;
+        int mid = 0;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (m[i] > arr2[mid]) {
+                left = mid + 1;
+            }
+            else if (m[i] < arr2[mid]) {
+                right = mid - 1;
+            }
+            else {
+                break;
+            }
+        }
+        printf("%d ", mid);
+    }
+
+    return 0;
+
 }
